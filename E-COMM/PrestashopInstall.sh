28f25e4c6db68aad56cc5b345f8a4ec0217622a9
Liam Browning <143126758+weumn00b@users.noreply.github.com>
2024-12-05 17:47:13 -0600

    Update PrestashopInstall.sh

--- E-COMM/PrestashopInstall.sh
+++ E-COMM/PrestashopInstall.sh
@@ -4,8 +4,7 @@ wget https://www.prestashop.com/download/old/prestashop_1.6.1.5.zip
 
 unzip prestashop_1.6.1.5.zip
 
-sudo chown -R apache: ~/prestashop/
 
-sudo mv ~/prestashop/* /var/www/html/
+sudo mv prestashop/* /var/www/html/
 
 sudo mv /var/www/html/admin/ /var/www/html/kdycau0197k8upr2/
