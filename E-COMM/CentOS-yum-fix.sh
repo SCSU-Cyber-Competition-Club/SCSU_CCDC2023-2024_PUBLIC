cb757e6578e0c6a383b9111576f655349ec02425
Liam Browning <limdork@gmail.com>
2024-12-05 15:04:44 -0600

    Adding script to fix yum base url

---
+++ E-COMM/CentOS-yum-fix.sh
@@ -0,0 +1,6 @@
+repo_file=/etc/yum.repos.d/CentOS-Base.repo
+cp $(repo_file) ~/CentOS-Base.repo.backup
+sudo sed -i s/#baseurl/baseurl $(repo_file)
+sudo sed -i s/mirrorlist.centos.org/vault.centos.org/ $(repo_file)
+sudo sed -i s/mirror.centos.org/vault.centos.org/ $(repo_file)
+sudo yum clean all
