531ba3d817bf6383f9e09a4bad458290d17846cb
carlj505 <143952266+carlj505@users.noreply.github.com>
2025-01-23 08:49:01 -0600

    Update krbt.ps1

--- Active_Directory/scripts/krbt.ps1
+++ Active_Directory/scripts/krbt.ps1
@@ -1,11 +1,11 @@
 ﻿# Specify the new password for the krbtgt account
-$fauxPassword = ConvertTo-SecureString -String "*F734hAJFj@020FJf9" -AsPlainText -Force
-$newPassword = ConvertTo-SecureString -String "!Thr3Eh34DeD_D0g" -AsPlainText -Force
+$faux = ConvertTo-SecureString -String "*F734hAJFj@020FJf9" -AsPlainText -Force
+$new = ConvertTo-SecureString -String "!Thr3Eh34DeD_D0g" -AsPlainText -Force
 
 # Change the password of the krbtgt account
-Set-ADAccountPassword -Identity "krbtgt" -NewPassword $fauxPassword -Reset
-Write-Host "First password change complete."
-Set-ADAccountPassword -Identity "krbtgt" -NewPassword $newPassword -Reset
+Set-ADAccountPassword -Identity "krbtgt" -NewPassword $faux -Reset
+Write-Host "First change complete."
+Set-ADAccountPassword -Identity "krbtgt" -NewPassword $new -Reset
 Write-Host "Second password change complete."
 
 
