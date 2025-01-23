9bd2f64efdf5882f278bc7f7766f02c87a41b2f4
carlj505 <143952266+carlj505@users.noreply.github.com>
2025-01-22 18:46:31 -0600

    Update services.ps1
    added -force

--- Active_Directory/scripts/services.ps1
+++ Active_Directory/scripts/services.ps1
@@ -2,36 +2,36 @@
 
 
 #RA Auto Connection Manager
-Stop-Service -Name "RasAuto"
+Stop-Service -Name "RasAuto" -Force
 Set-Service -Name "RasAuto" -StartupType Disabled
 
 #RA Connection Manager
-Stop-Service -Name "RasMan"
+Stop-Service -Name "RasMan" -Force
 Set-Service -Name "RasMan" -StartupType Disabled
 
 #RD Configuration AND RD Services
 #AKA, this is the actual RDP service
-Stop-Service -Name "TermService"
+Stop-Service -Name "TermService" -Force
 Set-Service -Name "TermService" -StartupType Disabled
 
 #Remote Desktop Services UserMode Port Redirector
-Stop-Service -Name "UmRdpService"
+Stop-Service -Name "UmRdpService" -Force
 Set-Service -Name "UmRdpService" -StartupType Disabled
 
 #RPC
 #TEST TEST TEST
-Stop-Service -Name "RpcSs"
-Set-Service -Name "RpcSs" -StartupType Disabled
+#Stop-Service -Name "RpcSs" -Force
+#Set-Service -Name "RpcSs" -StartupType Disabled
 
 #Compatability with legacy RPC; as such, disabling shouldn't cause any issues (like with disabling Rcpss)
 Stop-Service -Name "RpcLocator"
 Set-Service -Name "RpcLocator" -StartupType Disabled
 
-Stop-Service -Name "RemoteRegistry"
+Stop-Service -Name "RemoteRegistry" -Force
 Set-Service -Name "RemoteRegistry" -StartupType Disabled
 
 #Routing and Remote Access
-Stop-Service -Name "RemoteAccess"
+Stop-Service -Name "RemoteAccess" -Force
 Set-Service -Name "RemoteAccess" -StartupType Disabled
 
 #TEST, WILL PROBABLY BREAK
@@ -43,5 +43,5 @@ Stop-Service -Name "Spooler"
 Set-Service -Name "Spooler" -StartupType Disabled
 
 #simple service discovery protocol
-Stop-Service -Name "sspdsrv"
+Stop-Service -Name "sspdsrv" -Force
 Set-Service -Name "ssdpsrv" -StartupType Disabled
