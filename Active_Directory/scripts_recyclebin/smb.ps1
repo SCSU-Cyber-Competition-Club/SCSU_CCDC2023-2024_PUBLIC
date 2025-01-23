a34a30869d31b4e48a133181771fc775b3db2c4d
carlj505 <143952266+carlj505@users.noreply.github.com>
2025-01-23 17:15:29 -0600

    Add files via upload

---
+++ Active_Directory/scripts_recyclebin/smb.ps1
@@ -0,0 +1,5 @@
+﻿Set-SmbServerConfiguration -EnableSMB1Protocol $false -Force
+Set-SmbServerConfiguration -EnableSMB2Protocol $false -Force
+Set-SmbServerConfiguration -EnableSMB3Protocol $false -Force
+Write-Host "SMB disabled through its configuration. Make sure to disable it through Services as well...."
+
