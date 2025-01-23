fe756b8f5c21318b2233a56b44caae253979a00d
carlj505 <143952266+carlj505@users.noreply.github.com>
2025-01-23 17:16:52 -0600

    Update unblock.ps1

--- Active_Directory/scripts/unblock.ps1
+++ Active_Directory/scripts/unblock.ps1
@@ -5,7 +5,6 @@ unblock-file ldap.ps1
 unblock-file pass.ps1
 unblock-file preauth.ps1
 unblock-file services.ps1
-unblock-file smb.ps1
 unblock-file vaccine.ps1
 unblock-file firewall.ps1
 unblock-file firewallTEST.ps1
