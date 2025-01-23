d16c20acfcb4fefdca74b3ef87b70d5eb686e7e4
carlj505 <143952266+carlj505@users.noreply.github.com>
2025-01-23 15:47:25 -0600

    Update firewall.ps1
    -both doesn't work apparently xdddd

--- Active_Directory/scripts/firewall.ps1
+++ Active_Directory/scripts/firewall.ps1
@@ -42,49 +42,78 @@ New-NetFirewallRule -DisplayName "Allow LDAP Inbound"  -Direction Inbound  -Prot
 #Realized at this point you can make one rule for both inbound and outbound with -Direction Both. Not taking the time to clean up the code above, even though I'm taking the time to write this comment. Deal with it.
 
 
-New-NetFirewallRule -DisplayName "Block WinRM"  -Direction Both  -Protocol Any -Action Block  -Service "winrm"  -Profile Any
-New-NetFirewallRule -DisplayName "Block WMI"  -Direction Both  -Protocol Any -Action Block  -Service "winmgmt"  -Profile Any
-New-NetFirewallRule -DisplayName "Block RPC"  -Direction Both  -Protocol Any -Action Block  -Service "rpcss"  -Profile Any
+New-NetFirewallRule -DisplayName "Block WinRM"  -Direction Outbound -Protocol Any -Action Block  -Service "winrm"  -Profile Any
+New-NetFirewallRule -DisplayName "Block WinRM"  -Direction Inbound  -Protocol Any -Action Block  -Service "winrm"  -Profile Any
+
+New-NetFirewallRule -DisplayName "Block WMI"  -Direction Outbound -Protocol Any -Action Block  -Service "winmgmt"  -Profile Any
+New-NetFirewallRule -DisplayName "Block WMI"  -Direction Inbound  -Protocol Any -Action Block  -Service "winmgmt"  -Profile Any
+
+New-NetFirewallRule -DisplayName "Block RPC"  -Direction Outbound -Protocol Any -Action Block  -Service "rpcss"  -Profile Any
+New-NetFirewallRule -DisplayName "Block RPC"  -Direction Inbound  -Protocol Any -Action Block  -Service "rpcss"  -Profile Any
 
 
-New-NetFirewallRule -DisplayName "Block Telnet"  -Direction Both  -Protocol Any -Action Block  -Service "Tlntsvr"  -Profile Any
+New-NetFirewallRule -DisplayName "Block Telnet"  -Direction Outbound -Protocol Any -Action Block  -Service "Tlntsvr"  -Profile Any
+New-NetFirewallRule -DisplayName "Block Telnet"  -Direction Inbound -Protocol Any -Action Block  -Service "Tlntsvr"  -Profile Any
 
 
-New-NetFirewallRule -DisplayName "Block SSH"  -Direction Both  -Protocol Any -Action Block  -Service "sshd"  -Profile Any
+
+New-NetFirewallRule -DisplayName "Block SSH"  -Direction Outbound -Protocol Any -Action Block  -Service "sshd"  -Profile Any
+New-NetFirewallRule -DisplayName "Block SSH"  -Direction Inbound  -Protocol Any -Action Block  -Service "sshd"  -Profile Any
 
 #ASK/TEST!!!!!!!!
-New-NetFirewallRule -DisplayName "Block HTTP"  -Direction Both  -Protocol Any -Action Block  -Service "http"  -Profile Any
+New-NetFirewallRule -DisplayName "Block HTTP"  -Direction Outbound  -Protocol Any -Action Block  -Service "http"  -Profile Any
+New-NetFirewallRule -DisplayName "Block HTTP"  -Direction Inbound  -Protocol Any -Action Block  -Service "http"  -Profile Any
 
-New-NetFirewallRule -DisplayName "Block Kerberos"  -Direction Both  -Protocol Any -Action Block  -Service "Kerb5"  -Profile Any
+New-NetFirewallRule -DisplayName "Block Kerberos"  -Direction Outbound  -Protocol Any -Action Block  -Service "Kerb5"  -Profile Any
+New-NetFirewallRule -DisplayName "Block Kerberos"  -Direction Inbound  -Protocol Any -Action Block  -Service "Kerb5"  -Profile Any
 
-New-NetFirewallRule -DisplayName "Block RPC"  -Direction Both  -Protocol Any -Action Block  -Service "Rpcss"  -Profile Any
+New-NetFirewallRule -DisplayName "Block RPC"  -Direction Outbound  -Protocol Any -Action Block  -Service "Rpcss"  -Profile Any
+New-NetFirewallRule -DisplayName "Block RPC"  -Direction Inbound  -Protocol Any -Action Block  -Service "Rpcss"  -Profile Any
 
-New-NetFirewallRule -DisplayName "Block Kerb Pass Changes"  -Direction Both  -Protocol Any -Action Block  -Service "kpasswd"  -Profile Any
+New-NetFirewallRule -DisplayName "Block Kerb Pass Changes"  -Direction Outbound  -Protocol Any -Action Block  -Service "kpasswd"  -Profile Any
+New-NetFirewallRule -DisplayName "Block Kerb Pass Changes"  -Direction Inbound  -Protocol Any -Action Block  -Service "kpasswd"  -Profile Any
 
-New-NetFirewallRule -DisplayName "Block LDAP"  -Direction Both  -Protocol Any -Action Block  -Service "ldap"  -Profile Any
+New-NetFirewallRule -DisplayName "Block LDAP"  -Direction Outbound  -Protocol Any -Action Block  -Service "ldap"  -Profile Any
+New-NetFirewallRule -DisplayName "Block LDAP"  -Direction Inbound  -Protocol Any -Action Block  -Service "ldap"  -Profile Any
 
 #----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-New-NetFirewallRule -DisplayName "Block RDP"  -Direction Both  -Protocol Any -Action Block  -Service "TermService"  -Profile Any
+New-NetFirewallRule -DisplayName "Block RDP"  -Direction Outbound  -Protocol Any -Action Block  -Service "TermService"  -Profile Any
+New-NetFirewallRule -DisplayName "Block RDP"  -Direction Inbound  -Protocol Any -Action Block  -Service "TermService"  -Profile Any
 
-New-NetFirewallRule -DisplayName "Block SMB"  -Direction Both  -Protocol Any -Action Block  -Service "LanManServer"  -Profile Any
-New-NetFirewallRule -DisplayName "Block FTP"  -Direction Both  -Protocol Any -Action Block  -Service "ftpsvc"  -Profile Any
+New-NetFirewallRule -DisplayName "Block SMB"  -Direction Outbound  -Protocol Any -Action Block  -Service "LanManServer"  -Profile Any
+New-NetFirewallRule -DisplayName "Block SMB"  -Direction Inbound  -Protocol Any -Action Block  -Service "LanManServer"  -Profile Any
+
+New-NetFirewallRule -DisplayName "Block FTP"  -Direction Outbound  -Protocol Any -Action Block  -Service "ftpsvc"  -Profile Any
+New-NetFirewallRule -DisplayName "Block FTP"  -Direction Inbound  -Protocol Any -Action Block  -Service "ftpsvc"  -Profile Any
 
 
-New-NetFirewallRule -DisplayName "Block SSDP"  -Direction Both  -Protocol Any -Action Block  -Service "ssdpsrv"  -Profile Any
+New-NetFirewallRule -DisplayName "Block SSDP"  -Direction Outbound  -Protocol Any -Action Block  -Service "ssdpsrv"  -Profile Any
+New-NetFirewallRule -DisplayName "Block SSDP"  -Direction Inbound  -Protocol Any -Action Block  -Service "ssdpsrv"  -Profile Any
 
 #Following rule blocks port 593, which is RPC over HTTPS (RPC is needed, but not this). We already have restrictions for RPC above, but can't hurt.
-New-NetFirewallRule -DisplayName "Block RPC thru HTTPS"  -Direction Both  -Protocol Any -Action Block  -LocalPort 593  -Profile Any
+New-NetFirewallRule -DisplayName "Block RPC thru HTTPS"  -Direction Outbound  -Protocol Any -Action Block  -LocalPort 593  -Profile Any
+New-NetFirewallRule -DisplayName "Block RPC thru HTTPS"  -Direction Inbound  -Protocol Any -Action Block  -LocalPort 593  -Profile Any
 
 
 #The following rules aim at blocking VNC traffic. There is no single VNC service - many iterations exist. As such, these rules will use file paths
 #for the various executable versions of VNC, as well as blocking the common port. (Again, as attackers may maliciously alter their port used, only the local port is specified.)
+New-NetFirewallRule -Name "Block VNC Port" -DisplayName "Block Remote Desktop App (mstsc)" -LocalPort 5900 -Enabled True -Action Block -Direction Outbound
+New-NetFirewallRule -Name "Block VNCTightServer" -DisplayName "Block Remote Desktop App (mstsc)" -Enabled True -Program "C:\Program Files\TightVNC\tvnserver.exe" -Action Block -Direction Outbound
+New-NetFirewallRule -Name "Block VNCTightViewer" -DisplayName "Block Remote Desktop App (mstsc)" -Enabled True -Program "C:\Program Files\TightVNC\tvnviewer.exe" -Action Block -Direction Outbound
+New-NetFirewallRule -Name "Block VNCRealServer" -DisplayName "Block Remote Desktop App (mstsc)" -Enabled True -Program "C:\Program Files\RealVNC\vncserver.exe" -Action Block -Direction Outbound
+New-NetFirewallRule -Name "Block VNCRealViewer" -DisplayName "Block Remote Desktop App (mstsc)" -Enabled True -Program "C:\Program Files\RealVNC\vncviewer.exe" -Action Block -Direction Outbound
+New-NetFirewallRule -Name "Block VNCUltraServer" -DisplayName "Block Remote Desktop App (mstsc)" -Enabled True -Program "C:\Program Files\UltraVNC\winvnc.exe" -Action Block -Direction Outbound
+New-NetFirewallRule -Name "Block VNCUltraViewer" -DisplayName "Block Remote Desktop App (mstsc)" -Enabled True -Program "C:\Program Files\UltraVNC\vncviewer.exe" -Action Block -Direction Outbound
+New-NetFirewallRule -Name "Block VNCTigerServer" -DisplayName "Block Remote Desktop App (mstsc)" -Enabled True -Program "C:\Program Files\TigerVNC\vncserver.exe" -Action Block -Direction Outbound
+New-NetFirewallRule -Name "Block VNCTigerViewer" -DisplayName "Block Remote Desktop App (mstsc)" -Enabled True -Program "C:\Program Files\TigerVNC\vncviewer.exe" -Action Block -Direction Outbound
+
 New-NetFirewallRule -Name "Block VNC Port" -DisplayName "Block Remote Desktop App (mstsc)" -LocalPort 5900 -Enabled True -Action Block
-New-NetFirewallRule -Name "Block VNCTightServer" -DisplayName "Block Remote Desktop App (mstsc)" -Enabled True -Program "C:\Program Files\TightVNC\tvnserver.exe" -Action Block
-New-NetFirewallRule -Name "Block VNCTightViewer" -DisplayName "Block Remote Desktop App (mstsc)" -Enabled True -Program "C:\Program Files\TightVNC\tvnviewer.exe" -Action Block
-New-NetFirewallRule -Name "Block VNCRealServer" -DisplayName "Block Remote Desktop App (mstsc)" -Enabled True -Program "C:\Program Files\RealVNC\vncserver.exe" -Action Block
-New-NetFirewallRule -Name "Block VNCRealViewer" -DisplayName "Block Remote Desktop App (mstsc)" -Enabled True -Program "C:\Program Files\RealVNC\vncviewer.exe" -Action Block
-New-NetFirewallRule -Name "Block VNCUltraServer" -DisplayName "Block Remote Desktop App (mstsc)" -Enabled True -Program "C:\Program Files\UltraVNC\winvnc.exe" -Action Block
-New-NetFirewallRule -Name "Block VNCUltraViewer" -DisplayName "Block Remote Desktop App (mstsc)" -Enabled True -Program "C:\Program Files\UltraVNC\vncviewer.exe" -Action Block
-New-NetFirewallRule -Name "Block VNCTigerServer" -DisplayName "Block Remote Desktop App (mstsc)" -Enabled True -Program "C:\Program Files\TigerVNC\vncserver.exe" -Action Block
-New-NetFirewallRule -Name "Block VNCTigerViewer" -DisplayName "Block Remote Desktop App (mstsc)" -Enabled True -Program "C:\Program Files\TigerVNC\vncviewer.exe" -Action Block
+New-NetFirewallRule -Name "Block VNCTightServer" -DisplayName "Block Remote Desktop App (mstsc)" -Enabled True -Program "C:\Program Files\TightVNC\tvnserver.exe" -Action Block -Direction Inbound
+New-NetFirewallRule -Name "Block VNCTightViewer" -DisplayName "Block Remote Desktop App (mstsc)" -Enabled True -Program "C:\Program Files\TightVNC\tvnviewer.exe" -Action Block -Direction Inbound
+New-NetFirewallRule -Name "Block VNCRealServer" -DisplayName "Block Remote Desktop App (mstsc)" -Enabled True -Program "C:\Program Files\RealVNC\vncserver.exe" -Action Block -Direction Inbound
+New-NetFirewallRule -Name "Block VNCRealViewer" -DisplayName "Block Remote Desktop App (mstsc)" -Enabled True -Program "C:\Program Files\RealVNC\vncviewer.exe" -Action Block -Direction Inbound
+New-NetFirewallRule -Name "Block VNCUltraServer" -DisplayName "Block Remote Desktop App (mstsc)" -Enabled True -Program "C:\Program Files\UltraVNC\winvnc.exe" -Action Block -Direction Inbound
+New-NetFirewallRule -Name "Block VNCUltraViewer" -DisplayName "Block Remote Desktop App (mstsc)" -Enabled True -Program "C:\Program Files\UltraVNC\vncviewer.exe" -Action Block -Direction Inbound
+New-NetFirewallRule -Name "Block VNCTigerServer" -DisplayName "Block Remote Desktop App (mstsc)" -Enabled True -Program "C:\Program Files\TigerVNC\vncserver.exe" -Action Block -Direction Inbound
+New-NetFirewallRule -Name "Block VNCTigerViewer" -DisplayName "Block Remote Desktop App (mstsc)" -Enabled True -Program "C:\Program Files\TigerVNC\vncviewer.exe" -Action Block -Direction Inbound
