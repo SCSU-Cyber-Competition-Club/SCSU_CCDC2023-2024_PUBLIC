cb2f8f24c7832a49a792711deb1e8ae8daaf4d29
carlj505 <143952266+carlj505@users.noreply.github.com>
2025-01-23 16:37:32 -0600

    Update unblock.ps1

--- Active_Directory/scripts/unblock.ps1
+++ Active_Directory/scripts/unblock.ps1
@@ -1,6 +1,5 @@
 #Comp environment disallows running unsigned scripts. Good policy to have, but gotta work around it.
 unblock-file epa.ps1
-unblock-file firewall.ps1
 unblock-file krbt.ps1
 unblock-file ldap.ps1
 unblock-file pass.ps1
@@ -8,3 +7,5 @@ unblock-file preauth.ps1
 unblock-file services.ps1
 unblock-file smb.ps1
 unblock-file vaccine.ps1
+unblock-file firewall.ps1
+unblock-file firewallTEST.ps1
