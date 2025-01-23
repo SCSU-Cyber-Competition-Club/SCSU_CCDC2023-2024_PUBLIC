dbd00af6910ec56be1f7ff597e9000ada463b891
carlj505 <143952266+carlj505@users.noreply.github.com>
2025-01-22 18:58:43 -0600

    Update smb.ps1

--- Active_Directory/scripts/smb.ps1
+++ Active_Directory/scripts/smb.ps1
@@ -1,6 +1,5 @@
 ﻿Set-SmbServerConfiguration -EnableSMB1Protocol $false -Force
 Set-SmbServerConfiguration -EnableSMB2Protocol $false -Force
-Write-Host "SMB versions 1 and 2 disabled."
-Set-ItemProperty -Path "HKLM:\System\CurrentControlSet\Services\LanmanWorkstation\Parameters" -Name "RequireSecuritySignature" -Value 1
-Set-ItemProperty -Path "HKLM:\System\CurrentControlSet\Services\LanmanWorkstation\Parameters" -Name "EnableSecuritySignature" -Value 1
-Write-Host "SMB signing enabled and enforced."
+Set-SmbServerConfiguration -EnableSMB3Protocol $false -Force
+Write-Host "SMB disabled through its configuration. Make sure to disable it through Services as well...."
+
