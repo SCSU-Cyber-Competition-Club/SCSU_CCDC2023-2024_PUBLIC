6132998a9851138ca61b1d180b28ec3eb172254a
carlj505 <143952266+carlj505@users.noreply.github.com>
2025-03-05 13:22:15 -0600

    Update smb.ps1

--- Active_Directory/scripts/smb.ps1
+++ Active_Directory/scripts/smb.ps1
@@ -1,5 +1,6 @@
-﻿Set-SmbServerConfiguration -EnableSMB1Protocol $false -Force
+Write-Host "Hardening SMB..." -ForeGroundColor Red
+ Set-SmbServerConfiguration -EnableSMB1Protocol $false -Force
 Set-SmbServerConfiguration -EnableSMB2Protocol $true -Force
 Set-SmbServerConfiguration –EncryptData $true
-Write-Host "SMB Hardened."
+Write-Host "SMB Hardened." -ForeGroundColor Red
 
