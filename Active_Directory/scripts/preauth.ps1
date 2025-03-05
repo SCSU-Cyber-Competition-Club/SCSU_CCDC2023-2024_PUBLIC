72dfac87ad262b6292543daeead74abdd63b9539
carlj505 <143952266+carlj505@users.noreply.github.com>
2025-03-05 13:20:45 -0600

    Update preauth.ps1

--- Active_Directory/scripts/preauth.ps1
+++ Active_Directory/scripts/preauth.ps1
@@ -1,1 +1,3 @@
-﻿Get-ADUSer -Filter 'DoesNotRequirePreAuth -eq $true ' | Set-ADAccountControl -doesnotrequirepreauth $false
+Write-Host "Dealing with no-pre-auth users..." -ForeGroundColor Red
+ Get-ADUSer -Filter 'DoesNotRequirePreAuth -eq $true ' | Set-ADAccountControl -doesnotrequirepreauth $false
+ Write-Host "All preauthentication has been purged." -ForeGroundColor Red
