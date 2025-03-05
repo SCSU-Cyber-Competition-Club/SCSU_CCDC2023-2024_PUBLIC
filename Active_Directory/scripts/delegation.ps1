aac15cdbb8b6745180caaf13ea3a5cfd4d53466e
carlj505 <143952266+carlj505@users.noreply.github.com>
2025-03-05 12:38:15 -0600

    Update and rename delegation.ps1 to delegation.ps1

---
+++ Active_Directory/scripts/delegation.ps1
@@ -0,0 +1,8 @@
+﻿#This is just going to disable TrustedForDelegation for user accounts, for now. Worried about breaking services if doing so for system accounts.
+$accounts = Get-ADUser -Filter {TrustedForDelegation -eq $true}
+Write-Host "'nUSERS WITH DELEGATION TRUST ENABLED: $accounts"
+
+$computers = Get-ADUser -Filter {TrustedForDelegation -eq $true}
+Write-Host "'nCOMPUTERS WITH DELEGATION TRUST ENABLED:
+
+Write-Host "'nDelegation enumeration complete. (If the competition has you stressed, go ahead and say that five times fast.)"
