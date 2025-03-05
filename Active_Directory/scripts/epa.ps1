2938730f7c23387d60b94afe8f231996bf2da212
carlj505 <143952266+carlj505@users.noreply.github.com>
2025-03-05 13:14:14 -0600

    Update epa.ps1

--- Active_Directory/scripts/epa.ps1
+++ Active_Directory/scripts/epa.ps1
@@ -1,8 +1,9 @@
-﻿Set-ItemProperty -Path "HKLM:\System\CurrentControlSet\Control\Lsa" -Name "LmCompatibilityLevel" -Value 5
+Write-Host "Implementing EPA..." -ForeGroundColor Red
+ 
+ Set-ItemProperty -Path "HKLM:\System\CurrentControlSet\Control\Lsa" -Name "LmCompatibilityLevel" -Value 5
 Write-Host "LMCompatability set to 5. NTLMv2 enforced."
 
 Set-ItemProperty -Path "HKLM:\System\CurrentControlSet\Control\Lsa" -Name "SuppressExtendedProtection" -Value 0
 Write-Host "SuppressExtendedProtection set to 0..."
 
-Set-ItemProperty -Path "HKLM:\System\CurrentControlSet\Control\Lsa\Kerberos" -Name "ExtendedProtection" -Value 1
-Write-Host "Kerberos Extended Protection enabled. Script complete."
+Write-Host "EPA implemented." -ForeGroundColor Red
