f4c4cf2c9d7f4b7ebcac823241b373e41a51681d
carlj505 <143952266+carlj505@users.noreply.github.com>
2025-03-05 12:32:43 -0600

    Update and rename SPNumerate.ps1 to SPNumerate.ps1

---
+++ Active_Directory/scripts/SPNumerate.ps1
@@ -0,0 +1,10 @@
+﻿# Retrieve all computers with non-null SPNs
+Get-ADComputer -Filter * -Property ServicePrincipalName | Where-Object { $_.ServicePrincipalName -ne $null }
+
+# Retrieve all users with non-null SPNs
+Get-ADUser -Filter * -Property ServicePrincipalName | Where-Object { $_.ServicePrincipalName -ne $null }
+
+# Retrieve all managed service accounts (MSAs) with non-null SPNs
+Get-ADServiceAccount -Filter * -Property ServicePrincipalName | Where-Object { $_.ServicePrincipalName -ne $null }
+
+Write-Host "'nThat's it for SPNs..."
