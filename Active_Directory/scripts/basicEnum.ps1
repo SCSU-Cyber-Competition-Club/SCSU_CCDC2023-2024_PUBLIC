0e15d047f706c9692b93e25ef6707731a9f44cbf
carlj505 <143952266+carlj505@users.noreply.github.com>
2025-03-07 19:05:42 -0800

    Add files via upload

---
+++ Active_Directory/scripts/basicEnum.ps1
@@ -0,0 +1,16 @@
+﻿function AddAuditToFile {
+    param
+    (
+        [Parameter(Mandatory=$true)]
+        [string]$path
+    )
+    Get-Acl $path -Audit | Format-List Path,AuditToString | Out-File -FilePath 'file_before.txt' -Width 200 -Append
+    $File_ACL = Get-Acl $path
+    $AccessRule = New-Object System.Security.AccessControl.FileSystemAuditRule("Everyone","ReadData, ExecuteFile, WriteData","none","none",”Success, Failure")
+    $File_ACL.AddAuditRule($AccessRule)
+    $File_ACL | Set-Acl $path
+    Get-Acl $path -Audit | Format-List Path,AuditToString | Out-File -FilePath 'file_after.txt' -Width 200 -Append
+    }
+
+AddAuditToFile -path ./creds_for_script.csv
+
