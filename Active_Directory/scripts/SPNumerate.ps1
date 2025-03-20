7c97f6e42b65d3790358633de9b10769fef93630
carlj505 <143952266+carlj505@users.noreply.github.com>
2025-03-20 17:08:37 -0500

    Update SPNumerate.ps1

--- Active_Directory/scripts/SPNumerate.ps1
+++ Active_Directory/scripts/SPNumerate.ps1
@@ -1,3 +1,9 @@
+# Service Principal Names (SPNs) are used to associate a service instance with a service account. In other words,
+#accounts with SPNs have access to the service relevant to that SPN. Knowledge of the SPNs present in your environment
+#is important for understanding your attack surface; finding an SPN for a service you really don't want compromised
+#on a low-privilege user account (or any account weakly protected) is something that needs to be remedied.
+
+#If we assume the competition doesn't change, there will be no unnecessary/misplaced SPNs, however.
 Write-Host "Initiating SPNumeration..." -ForeGroundColor Red
  # Retrieve all computers with non-null SPNs
 Get-ADComputer -Filter * -Property ServicePrincipalName | Where-Object { $_.ServicePrincipalName -ne $null }
