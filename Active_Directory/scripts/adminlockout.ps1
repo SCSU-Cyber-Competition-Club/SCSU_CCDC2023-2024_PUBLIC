8bc35e4aa81dcbba81959026a05d32f323a06983
carlj505 <143952266+carlj505@users.noreply.github.com>
2025-03-20 18:40:09 -0500

    Update adminlockout.ps1

--- Active_Directory/scripts/adminlockout.ps1
+++ Active_Directory/scripts/adminlockout.ps1
@@ -1,3 +1,5 @@
+# When a lockout policy is enabled through Group Policy, it does NOT apply to administrator accounts, unless Admin Lockout is enabled. This doesn't show up sometime in
+#GP... but enabling it via Registry should get ya set either way. (Your Group Policy Editor is mostly a glorified GUI for editing Registry keys.)
 Write-Host "Enabling admin lockout..." -ForeGroundColor Red
 Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name "EnableAdminAccountLock" -Value 1
 Write-Host "Admin lockout enabled." -ForeGroundColor Red
