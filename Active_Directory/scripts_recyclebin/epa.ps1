d3a49a045b76c8259fb91386befb565a1789f867
carlj505 <143952266+carlj505@users.noreply.github.com>
2025-03-25 18:02:01 -0500

    Update and rename epa.ps1 to epa.ps1

--- Active_Directory/scripts_recyclebin/epa.ps1
+++ Active_Directory/scripts_recyclebin/epa.ps1
@@ -1,3 +1,6 @@
+# Setting these values implements "EPA", which forces NTLMv2 to be used over NTLM.
+# This is recycled as we just want to use Kerberos authentication over NTLMv2 authentication, which is much weaker.
+# You would only use NTLMv2 in an environment with old-ass systems/operating systems which need the legacy NTLM/NTLMv2 protocol.
 Write-Host "Implementing EPA..." -ForeGroundColor Red
  
  Set-ItemProperty -Path "HKLM:\System\CurrentControlSet\Control\Lsa" -Name "LmCompatibilityLevel" -Value 5
