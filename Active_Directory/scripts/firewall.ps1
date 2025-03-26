904e34536e189875567c2a84c9a5c4a7d43bf419
carlj505 <143952266+carlj505@users.noreply.github.com>
2025-03-26 14:22:56 -0500

    Update and rename firewall2.ps1 to firewall.ps1

--- Active_Directory/scripts/firewall.ps1
+++ Active_Directory/scripts/firewall.ps1
@@ -1,5 +1,6 @@
 ﻿Write-Host "Building firewall..." -ForeGroundColor Red
 
+# Risky services that can be blocked netcon-wise outright without causing issues in our environment.
 New-NetFirewallRule -DisplayName "Block Telnet"  -Direction Outbound -Protocol Any -Action Block  -Service "Tlntsvr"  -Profile Any
 New-NetFirewallRule -DisplayName "Block Telnet"  -Direction Inbound -Protocol Any -Action Block  -Service "Tlntsvr"  -Profile Any
 
@@ -16,18 +17,21 @@ New-NetFirewallRule -DisplayName "Block SSDP"  -Direction Inbound  -Protocol Any
 
 
 
+# 135: RPC (Remote Procedure Call), 137: NetBIOS, 139: SMB over NetBIOS, 445: SMB
+# The above four are dangerous ports, especially with Zerologon (which our system won't/can't have received the patch for);
+# However, as a domain controller, your system NEEDS to send data OUT to the domain through these ports.
+# In our environment (especially considering there's only one domain controller), this traffic can be blocked INBOUND without interfering with scoring.
+New-NetFirewallRule -DisplayName "Block 135 TCP (Inbound only)"  -Direction Inbound  -Protocol Any  -LocalPort 135 -Action Block -Profile Any
 
-New-NetFirewallRule -DisplayName "Block 135 TCP (Inbound only)"  -Direction Inbound  -Protocol TCP  -LocalPort 135 -Action Block -Profile Any
+New-NetFirewallRule -DisplayName "Block 137 TCP (Inbound only)"  -Direction Inbound  -Protocol Any  -LocalPort 137 -Action Block -Profile Any
 
-New-NetFirewallRule -DisplayName "Block 137 TCP (Inbound only)"  -Direction Inbound  -Protocol TCP  -LocalPort 137 -Action Block -Profile Any
+New-NetFirewallRule -DisplayName "Block 139 TCP (Inbound only)"  -Direction Inbound  -Protocol Any  -LocalPort 139 -Action Block -Profile Any
 
-New-NetFirewallRule -DisplayName "Block 139 TCP (Inbound only)"  -Direction Inbound  -Protocol TCP  -LocalPort 139 -Action Block -Profile Any
+New-NetFirewallRule -DisplayName "Block 445 TCP (Inbound only)"  -Direction Inbound  -Protocol Any  -LocalPort 445 -Action Block -Profile Any
 
-New-NetFirewallRule -DisplayName "Block 445 TCP (Inbound only)"  -Direction Inbound  -Protocol TCP  -LocalPort 445 -Action Block -Profile Any
+New-NetFirewallRule -DisplayName "Block HTTP TCP (Inbound only)"  -Direction Inbound  -Protocol Any -Action Block  -LocalPort 80  -Profile Any
 
-New-NetFirewallRule -DisplayName "Block HTTP TCP (Inbound only)"  -Direction Inbound  -Protocol TCP -Action Block  -LocalPort 80  -Profile Any
-
-New-NetFirewallRule -DisplayName "Block HTTPS TCP (Inbound only)"  -Direction Inbound  -Protocol TCP -Action Block  -LocalPort 443  -Profile Any
+New-NetFirewallRule -DisplayName "Block HTTPS TCP (Inbound only)"  -Direction Inbound  -Protocol Any -Action Block  -LocalPort 443  -Profile Any
 
 
 New-NetFirewallRule -DisplayName "Block 135 UDP (Inbound only)"  -Direction Inbound  -Protocol UDP  -LocalPort 135 -Action Block -Profile Any
@@ -42,6 +46,7 @@ New-NetFirewallRule -DisplayName "Block HTTP UDP (Inbound only)"  -Direction Inb
 
 New-NetFirewallRule -DisplayName "Block HTTPS UDP (Inbound only)"  -Direction Inbound  -Protocol UDP -Action Block  -LocalPort 443  -Profile Any
 
+# Creating rules for a firewall doesn't mean the firewall is enabled. In previous competitions, the firewalls have been disabled initially.
 Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled True
 
 Write-Host "Firewall built (and enabled)."-ForeGroundColor Red
