246c48bb425aae7061690e20daba16c76dc81165
carlj505 <143952266+carlj505@users.noreply.github.com>
2025-03-27 08:25:54 -0500

    Update llmnr.ps1

--- Active_Directory/scripts/llmnr.ps1
+++ Active_Directory/scripts/llmnr.ps1
@@ -1,3 +1,5 @@
+# Link Local Multicast Name Resolution is a legacy protocol for resolving hostnames on a network; DNS performs this function,
+# without LLMNR's vulnerabilities to poisoning attacks (similar to NBT-NS poisoning).
 Write-Host "Disabling LLMNR..." -ForeGroundColor Red
 Set-ItemProperty -Path "HKLM\SOFTWARE\Policies\Microsoft\Windows NT\DNSClient" -Name "EnableMulticast" -Value 0
 Write-Host "LLMNR disabled." -ForeGroundColor Red
