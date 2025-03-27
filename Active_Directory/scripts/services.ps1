251629a09df6adc7458276d42cae59c32e8633d0
carlj505 <143952266+carlj505@users.noreply.github.com>
2025-03-27 09:10:52 -0500

    Update services.ps1

--- Active_Directory/scripts/services.ps1
+++ Active_Directory/scripts/services.ps1
@@ -1,11 +1,22 @@
-﻿#Service names were provided by ChatGPT. These need to be tested before used in comp.
+#Risky services that need to be disabled (and can be).
+#I'm going to leave a few in here that are risky, but CAN'T be disabled without breaking our environment to save you the time
+#of finding these while doing your own research and ultimately seeing they sholudn't be disabled.
+#These will be commented out. If you want to help other teams shoot themselves in the foot, remove these from the script
+#before you have to make the Github public to others, in case they were planning on disabling them.
+
+#READ THIS!
+#Disabling Remote Registry will cause some scary-looking errors in your Server Manager, particularly concerning DFS Namespace.
+#This will break the ability for file shares in your environment. HOWEVER, the domain will keep limping on as much as we need it to
+#for the competition.
+#If you happen to get some inject requiring setting up a file share, remember to reenable and start (easy in the Services GUI).
+Stop-Service -Name "RemoteRegistry" -Force
+Set-Service -Name "RemoteRegistry" -StartupType Disabled
+
 
 Write-Host "Disabling naughty services..." -ForeGroundColor Red
 #RA Auto Connection Manager
 Stop-Service -Name "RasAuto" -Force
 Set-Service -Name "RasAuto" -StartupType Disabled
-
-
 
 #RD Configuration AND RD Services
 #AKA, this is the actual RDP service
@@ -16,12 +27,13 @@ Set-Service -Name "TermService" -StartupType Disabled
 Stop-Service -Name "UmRdpService" -Force
 Set-Service -Name "UmRdpService" -StartupType Disabled
 
-#RPC
-#TEST TEST TEST
+#Remote Procedure Call (RPC)
+#This isn't just required for domain controller functionality, this is required for WINDOWS functionality.
+#Even disabling this on your Windows desktop sitting at home will cause a lot of issues.
 #Stop-Service -Name "RpcSs" -Force
 #Set-Service -Name "RpcSs" -StartupType Disabled
 
-#Compatability with legacy RPC; as such, disabling shouldn't cause any issues (like with disabling Rcpss)
+#Compatability with LEGACY RPC; as such, disabling is fine in our environment.
 #Stop-Service -Name "RpcLocator"
 #Set-Service -Name "RpcLocator" -StartupType Disabled
 
@@ -29,7 +41,7 @@ Set-Service -Name "UmRdpService" -StartupType Disabled
 Stop-Service -Name "RemoteAccess" -Force
 Set-Service -Name "RemoteAccess" -StartupType Disabled
 
-#TEST, WILL PROBABLY BREAK
+#Definitely an attack vector, but needed for a domain controller.
 #Stop-Service -Name "WinMgmt"
 #Set-Service -Name "WinMgmt" -StartupType Disabled
 
@@ -38,6 +50,7 @@ Stop-Service -Name "Spooler"
 Set-Service -Name "Spooler" -StartupType Disabled
 
 #SMB
+#Again, huge attack vector, but needed as DC. See smb.ps1 for how we utilize SMB as safely as possible.
 #Stop-Service -Name "LanmanServer" -Force
 #Set-Service -Name "LanmanServer" -StartupType Disabled
 #Stop-Service -Name "LanmanWorkstation" -Force
