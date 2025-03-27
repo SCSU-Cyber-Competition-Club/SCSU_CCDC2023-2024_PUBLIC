cc4916bec10a443b915341e6059a86f59af935e2
carlj505 <143952266+carlj505@users.noreply.github.com>
2025-03-27 08:37:46 -0500

    Update preauth.ps1

--- Active_Directory/scripts/preauth.ps1
+++ Active_Directory/scripts/preauth.ps1
@@ -1,3 +1,10 @@
-Write-Host "Dealing with no-pre-auth users..." -ForeGroundColor Red
+# Allowing an account to not require pre-authorization allows attackers to easily obtain the password hash of an account early
+# in the Kerberos authentication process through AS-REP roasting, which can be cracked offline.
+
+#Find all accounts who don't require pre-authentication; pipe these accounts into a command making them require it.
+Write-Host "Dealing with no-pre-auth accounts..." -ForeGroundColor Red
+ 
  Get-ADUSer -Filter 'DoesNotRequirePreAuth -eq $true ' | Set-ADAccountControl -doesnotrequirepreauth $false
+ Get-ADComputer -Filter 'DoesNotRequirePreAuth -eq $true ' | Set-ADAccountControl -doesnotrequirepreauth $false
+# Get-ADUSer -Filter 'DoesNotRequirePreAuth -eq $true ' | Set-ADAccountControl -doesnotrequirepreauth $false
  Write-Host "All preauthentication has been purged." -ForeGroundColor Red
