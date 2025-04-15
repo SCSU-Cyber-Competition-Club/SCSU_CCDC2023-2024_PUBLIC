aa191f0b54adfd3f791332e3679c47a79fe6e3c9
carlj505 <143952266+carlj505@users.noreply.github.com>
2025-04-15 16:50:54 -0500

    Add files via upload

---
+++ Active_Directory/scripts/DC/control.ps1
@@ -0,0 +1,19 @@
+#This script will not be needed if we fully realize the vision of Honorable Monkey. (This justs executes all of the scripts.)
+#If this vision is not realized (or it is not ready yet by the time of your competition), two things are required of you:
+#1. Add lines for all of the hardening scripts you're bringing into the comp, and remove ones for scripts that might be here but aren't being used anymore
+#2. Ensure this script is in the same directory as the hardening scripts
+& "./delegation.ps1"
+& "./SPNumerate.ps1"
+& "./adminlockout.ps1"
+& "./epa.ps1"
+& "./firewall2.ps1"
+& "./preauth.ps1"
+& "./llmnr.ps1"
+& "./smb.ps1"
+& "./netbios.ps1"
+& "./basicEnum.ps1"
+& "./services.ps1"
+& "./sysminstall.ps1"
+
+
+
