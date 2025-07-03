a4feabeb75f53e13053e37a792a95fb7a3a94fe3
carlj505 <143952266+carlj505@users.noreply.github.com>
2025-07-02 19:08:01 -0500

    Delete Active_Directory/scriptsInjects directory
    Redundant; scripts within are located elsewhere.

--- Active_Directory/scriptsInjects/README.md
+++
@@ -1,1 +0,0 @@
-In progress, and requiring testing. Scripts will be placed into a diferent, appropriate folder after completion (one will be created for DC's, one for workstations).
--- Active_Directory/scriptsInjects/loginBanner.ps1
+++
@@ -1,3 +0,0 @@
-# Windows Login Banners
-Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name "legalnoticecaption" -Value "Attention!"
-Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name "legalnoticetext" -Value "All network activity is monitored. Unauthorized usage of company systems constitutes a federal crime, and will lead to criminal prosecution."
