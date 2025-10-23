53ac62b7787d395f73fcb4208bad920e1a245049
carlj505 <143952266+carlj505@users.noreply.github.com>
2025-10-23 14:06:42 -0500

    Update and rename Firewall/2026/windows_wkst/control.ps1 to Firewall/2026/windows_wkst/hardening/control.ps1

--- Firewall/2026/windows_wkst/hardening/control.ps1
+++ Firewall/2026/windows_wkst/hardening/control.ps1
@@ -2,18 +2,14 @@
 #If this vision is not realized (or it is not ready yet by the time of your competition), two things are required of you:
 #1. Add lines for all of the hardening scripts you're bringing into the comp, and remove ones for scripts that might be here but aren't being used anymore
 #2. Ensure this script is in the same directory as the hardening scripts
-& "./delegation.ps1"
-& "./SPNumerate.ps1"
-& "./adminlockout.ps1"
-& "./epa.ps1"
-& "./firewall2.ps1"
-& "./preauth.ps1"
+& "./firewall.ps1"
+& "./KerbLogging.ps1"
 & "./llmnr.ps1"
-& "./smb.ps1"
 & "./netbios.ps1"
-& "./basicEnum.ps1"
 & "./services.ps1"
+& "./smb_client.ps1"
 & "./sysminstall.ps1"
+
 
 
 
