4441ed53e5c820cef8acecc372b12ebf537c0d16
carlj505 <143952266+carlj505@users.noreply.github.com>
2025-10-30 15:45:25 -0500

    Update firewall.ps1

--- Active_Directory/scripts/Workstation/hardening/firewall.ps1
+++ Active_Directory/scripts/Workstation/hardening/firewall.ps1
@@ -14,21 +14,22 @@ New-NetFirewallRule -DisplayName "Block SSDP"  -Direction Outbound -Protocol Any
 New-NetFirewallRule -DisplayName "Block SSDP"  -Direction Inbound  -Protocol Any -Action Block  -Service "ssdpsrv"  -Profile Any
 
 
-New-NetFirewallRule -DisplayName "Block HTTP TCP (Inbound only)"  -Direction Inbound  -Protocol TCP -Action Block  -LocalPort 80  -Profile Any
+#New-NetFirewallRule -DisplayName "Block HTTP TCP (Inbound only)"  -Direction Inbound  -Protocol TCP -Action Block  -LocalPort 80  -Profile Any
 
-New-NetFirewallRule -DisplayName "Block HTTPS TCP (Inbound only)"  -Direction Inbound  -Protocol TCP -Action Block  -LocalPort 443  -Profile Any
-
-
+#New-NetFirewallRule -DisplayName "Block HTTPS TCP (Inbound only)"  -Direction Inbound  -Protocol TCP -Action Block  -LocalPort 443  -Profile Any
 
 
-New-NetFirewallRule -DisplayName "Block 445 UDP (Inbound only)"  -Direction Inbound  -Protocol UDP  -LocalPort 445 -Action Block -Profile Any
 
-New-NetFirewallRule -DisplayName "Block HTTP UDP (Inbound only)"  -Direction Inbound  -Protocol UDP -Action Block  -LocalPort 80  -Profile Any
 
-New-NetFirewallRule -DisplayName "Block HTTPS UDP (Inbound only)"  -Direction Inbound  -Protocol UDP -Action Block  -LocalPort 443  -Profile Any
+#New-NetFirewallRule -DisplayName "Block 445 UDP (Inbound only)"  -Direction Inbound  -Protocol UDP  -LocalPort 445 -Action Block -Profile Any
+
+#New-NetFirewallRule -DisplayName "Block HTTP UDP (Inbound only)"  -Direction Inbound  -Protocol UDP -Action Block  -LocalPort 80  -Profile Any
+
+#New-NetFirewallRule -DisplayName "Block HTTPS UDP (Inbound only)"  -Direction Inbound  -Protocol UDP -Action Block  -LocalPort 443  -Profile Any
 
 
 # Creating rules for a firewall doesn't mean the firewall is enabled. In previous competitions, the firewalls have been disabled initially.
 Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled True
 
 Write-Host "Firewall built (and enabled)."-ForeGroundColor Red
+
