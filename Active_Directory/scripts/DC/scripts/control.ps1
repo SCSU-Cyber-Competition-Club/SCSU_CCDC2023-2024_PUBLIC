71db0b5ef121cffdb287ac1864d65935818d1e43
carlj505 <143952266+carlj505@users.noreply.github.com>
2025-10-30 17:35:17 -0500

    Update control.ps1

--- Active_Directory/scripts/DC/scripts/control.ps1
+++ Active_Directory/scripts/DC/scripts/control.ps1
@@ -3,15 +3,11 @@
 #1. Add lines for all of the hardening scripts you're bringing into the comp, and remove ones for scripts that might be here but aren't being used anymore
 #2. Ensure this script is in the same directory as the hardening scripts
 & "./delegation.ps1"
-& "./SPNumerate.ps1"
 & "./adminlockout.ps1"
-& "./epa.ps1"
-& "./firewall2.ps1"
+& "./firewall.ps1"
 & "./preauth.ps1"
 & "./llmnr.ps1"
-& "./smb.ps1"
 & "./netbios.ps1"
-& "./basicEnum.ps1"
 & "./services.ps1"
 & "./sysminstall.ps1"
 
