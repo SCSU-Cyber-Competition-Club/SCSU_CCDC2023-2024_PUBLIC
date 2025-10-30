8c6a2b291f3dbe637384c1b67c42f0db4362868e
carlj505 <143952266+carlj505@users.noreply.github.com>
2025-10-30 15:47:22 -0500

    Update control.ps1

--- Firewall/2026/windows_wkst/hardening/control.ps1
+++ Firewall/2026/windows_wkst/hardening/control.ps1
@@ -2,7 +2,7 @@
 #If this vision is not realized (or it is not ready yet by the time of your competition), two things are required of you:
 #1. Add lines for all of the hardening scripts you're bringing into the comp, and remove ones for scripts that might be here but aren't being used anymore
 #2. Ensure this script is in the same directory as the hardening scripts
-& "./firewall.ps1"
+#& "./firewall.ps1"
 & "./KerbLogging.ps1"
 & "./llmnr.ps1"
 & "./netbios.ps1"
